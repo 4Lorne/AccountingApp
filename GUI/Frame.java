9c59af60d56497ee8aa9e96d1883cc738bfb7eaf
4Lorne <lornedayy@gmail.com>
2022-11-08 16:52:55 -0400

    First page of GUI working with GridBag

--- GUI/Frame.java
+++ GUI/Frame.java
@@ -7,31 +7,50 @@ import java.awt.event.ActionListener;
 
 
 public class Frame extends JFrame {
-    JLabel select = new JLabel("Select student or staff.");
-    JPanel panel = new JPanel();
-    JButton student = new JButton("Student");
-    JButton staff = new JButton("Staff");
-    JButton exit = new JButton("Exit");
-    BorderLayout borderLayout = new BorderLayout(20,30);
+    JPanel panel = new JPanel(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
 
     public Frame(){
-        //Buttons need to be added to the panel before being
-        //Added to the frame.
-        setLayout(borderLayout);
-        panel.setLayout(borderLayout);
+        setTitle("Accounting Application");
+        //Size of the frame
         setSize(300,200);
 
+        //NOTE
+        //Buttons need to be added to the panel before being
+        //Added to the frame.
+
+        //Buttons will grow with the screen
+        c.weightx=0.5;
+        c.insets= new Insets(5,5,5,5);
+
+        //Adding label to the panel
+        JLabel select = new JLabel("Select student or staff.");
+        c.fill=GridBagConstraints.CENTER;
+        //Sets number of cells in x row to 0, centering the label
+        c.gridwidth=0;
+        c.gridx=0;
+        c.gridy=0;
+        panel.add(select,c);
+
         //Adding the buttons to the panel
-        panel.add(select);
-        panel.add(student);
-        panel.add(staff);
-        panel.add(exit);
+        JButton student = new JButton("Student");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth=1;
+        c.gridx=0;
+        c.gridy=5;
+        panel.add(student,c);
 
-        student.setBounds(20,100,80,30);
-        staff.setBounds(100,100,80,30);
-        select.setBounds(150,0,150,100);
-        exit.setBounds(250,100,80,30);
+        JButton staff = new JButton("Staff");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx=1;
+        c.gridy=5;
+        panel.add(staff,c);
 
+        JButton exit = new JButton("Exit");
+        c.fill=GridBagConstraints.HORIZONTAL;
+        c.gridx=2;
+        c.gridy=5;
+        panel.add(exit,c);
 
         //Adding the panel to the frame
         add(panel);
